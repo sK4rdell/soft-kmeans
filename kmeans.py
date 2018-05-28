f07bef0163a42bdd37579be82a115bd584e5a743
Simon Kardell <simonkardell@gmail.com>
2018-05-28 13:28:11 +0200

    fixed bug

--- kmeans.py
+++ kmeans.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 def plot_k_means(x, r, k):
 
     random_colors = np.random.random((k, 3))
-    colors = r * random_colors
+    colors = r.dot(random_colors)
+    print(r[:20])
     plt.scatter(x[:,0], x[:,1], c=colors)
     plt.show()
 
@@ -22,50 +23,51 @@ def initialize_centers(x, num_k):
         centers[k] = x[idx]
     return centers
 
-def update_centers(r, x, K):
+def update_centers(x, r, K):
     N, D = x.shape
-    centers = np.zeros((num_k, D))
+    centers = np.zeros((K, D))
     for k in range(K):
-        centers[k] = ( (x * r[:, k] ) / r[:, k].sum()).sum()
+        centers[k] = r[:, k].dot(x) / r[:, k].sum()
     return centers
 
-def cost():
-    pass
+def square_dist(a, b):
+    return (a - b) ** 2
+
+def cost_func(x, r, centers, K):
+    
+    cost = 0
+    for k in range(K):
+        norm = np.linalg.norm(x - centers[k], 2)
+        cost += (norm * np.expand_dims(r[:, k], axis=1) ).sum()
+    return cost
+
 
 def cluster_responsibilities(centers, x, beta):
     N, _ = x.shape
     K, D = centers.shape
     R = np.zeros((N, K))
 
-    for n in range(N):
-        R[n] = np.exp(-beta * np.linalg.norm(centers - x[n], 2))
-
+    for n in range(N):        
+        R[n] = np.exp(-beta * np.linalg.norm(centers - x[n], 2, axis=1)) 
     R /= R.sum(axis=1, keepdims=True)
 
     return R
 
-def soft_k_means(x, num_k, max_iters=20, beta=1.):
-    centers = initialize_centers(x, num_k)
+def soft_k_means(x, K, max_iters=20, beta=1.):
+    centers = initialize_centers(x, K)
     prev_cost = 0
     for _ in range(max_iters):
-
         r = cluster_responsibilities(centers, x, beta)
-
-        centers = update_centers(r, x, num_k)
-        cost = cost(x, centers)
-        if cost == prev_cost:
+        centers = update_centers(x, r, K)
+        cost = cost_func(x, r, centers, K)
+        if np.abs(cost - prev_cost) < 1e-5:
             break
         prev_cost = cost
         
-    plot_k_means(x, r, num_k)
+    plot_k_means(x, r, K)
 
 
-
-
-def main():
-    dim = 2
-    dist = 4
-    std = 1
+def generate_samples(std=1, dim=2, dist=4):
     mu0 = np.array([0,0])
     mu1 = np.array([dist, dist])
     mu2 = np.array([0, dist])
@@ -75,8 +77,12 @@ def main():
     x1 = np.random.randn(Nc, dim) * std + mu1
     x2 = np.random.randn(Nc, dim) * std + mu2
     x = np.concatenate((x0, x1, x2), axis=0)
+    return x
     
-    soft_k_means(x, num_k=3)
+
+def main():
+    x = generate_samples()
+    soft_k_means(x, K=3)
     
 
 if __name__ == "__main__":
